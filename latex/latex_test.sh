5f7876fd421ddedc12b733acebc7395a76afa122
prashantsinalkar <prashantsinalkar@gmail.com>
2018-11-05 16:04:04 +0530

    added width as 5 inch for image

--- latex/latex_test.sh
+++ latex/latex_test.sh
@@ -144,7 +144,7 @@ echo $col6 > $CURDIR/Figure_files
 		 if [[ $(file -b ../$col6) =~ JPEG || $(file -b ../$col6) =~ PNG || $(file -b ../$col6) =~ JPG  ]]; then
 		     echo \\curlable{Fig~$col3} >> $CURDIR/TEX
 		     echo \\begin{figure} >> $CURDIR/TEX
-		     echo \\includegraphics[scale=0.3]{../$col6}  >> $CURDIR/TEX
+		     echo \\includegraphics[width=5in]{../$col6}  >> $CURDIR/TEX
 		     echo \\caption{$col4} >> $CURDIR/TEX
 		     echo \\end{figure} >> $CURDIR/TEX
 		     echo >> $CURDIR/TEX
@@ -172,7 +172,7 @@ echo $col2 > $CURDIR/image_files
 	then
 	       	 echo \\begin{figure} >> $CURDIR/TEX
                  echo \\label{AP:$col4} >> $CURDIR/TEX           
-                 echo \\includegraphics[scale=0.3]{../$col2}  >> $CURDIR/TEX
+                 echo \\includegraphics[width=5in]{../$col2}  >> $CURDIR/TEX
                  echo \\tcaption {$col3}{$col3} >> $CURDIR/TEX          
                  echo \\end{figure} >> $CURDIR/TEX
                  echo >> $CURDIR/TEX
